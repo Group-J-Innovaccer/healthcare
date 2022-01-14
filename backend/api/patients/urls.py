9540ab34ddfd2c1cf03d74d5b7777023d37ef337
Ritik_Jain <rjritikjain330@gmail.com>
2022-01-14 17:45:54 +0530

    Doctor can view patient list through GET API

--- backend/api/patients/urls.py
+++ backend/api/patients/urls.py
@@ -7,4 +7,6 @@ from .models import Patient
 
 urlpatterns = [
     path('list/', views.PatientList.as_view()),
+    path('list/(?P<pk>[0-9]+)$', views.patient_detail),
+    # url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
 ]
--- backend/api/patients/views.py
+++ backend/api/patients/views.py
@@ -5,6 +5,9 @@ from .serializers import PatientSerializer
 from .models import Patient
 from rest_framework import generics
 from rest_framework import mixins
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework.decorators import api_view
 
 # Patient list for doctor get API
 class PatientList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -19,7 +22,35 @@ class PatientList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateM
 
 
 # Create new Patient PostAPI (doctor)
+    # @api_view(['POST'])
+    # def addPatientByDoctor(request):
+    #     return self.create(request)
 
 # Patient Detail for patient get API (doctor)
+    # @api_view(['GET'])
+    # def patientListViewDoctor(request):
+    #     if request.method == 'GET':
+    #         queryset=Patient.objects.all
 
 # Edit Patient Detail PutAPI (doctor)
+    # @api_view(['PUT'])
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def patient_detail(request, pk):
+    try: 
+        patient = Patient.objects.get(pk=pk) 
+    except Patient.DoesNotExist: 
+        return JsonResponse({'message': 'The {patient} does not exist'}) 
+            
+    if request.method == 'GET': 
+        patient_serializer = PatientSerializer(patient) 
+        return JsonResponse(PatientSerializer.data) 
+            
+    elif request.method == 'PUT': 
+        patient_data = JSONParser().parse(request) 
+        patient_serializer = PatientSerializer(patient, data=patient_data) 
+        if patient_serializer.is_valid(): 
+            patient_serializer.save() 
+            return JsonResponse(patient_serializer.data) 
+        return JsonResponse(patient_serializer.errors)
