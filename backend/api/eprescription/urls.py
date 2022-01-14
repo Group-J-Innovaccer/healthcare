ee60bda84ef969be2d3597f6a7f08265a59780cc
Shallum99 <shallumisrael@gmail.com>
2022-01-14 19:40:25 +0530

    Fetched data through primary key via medication api

--- backend/api/eprescription/urls.py
+++ backend/api/eprescription/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('prescription/', views.PrescriptionList.as_view()),
     path('patientprescription/', views.PatientPrescriptionList.as_view()),
     path('medication/', views.medication_list),
-    path('medication/(?P<pk>[a-z]+)$', views.medication_detail),
+    path('medication/<int:pk>', views.medication_detail),
     
 ]
