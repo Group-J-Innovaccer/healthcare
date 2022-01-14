2071e4493d9b044d102abc2a5bac2fcece27c074
Santhosh Puvaneswaran <santhosh.puvaneswaran@gmail.com>
2022-01-14 23:47:52 +0530

    added the views.py file

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DoctorSerializer
+from .models import Doctor
+from rest_framework import generics
+from rest_framework import mixins 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+
+class DoctorList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
