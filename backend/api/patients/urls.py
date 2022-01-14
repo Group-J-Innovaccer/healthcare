ee39b405cb5eda812e6768f209f1a4b627cef5cf
Ritik_Jain <rjritikjain330@gmail.com>
2022-01-14 17:53:26 +0530

    Modified url.py and views.py

--- backend/api/patients/urls.py
+++ backend/api/patients/urls.py
@@ -7,6 +7,4 @@ from .models import Patient
 
 urlpatterns = [
     path('list/', views.PatientList.as_view()),
-    path('list/(?P<pk>[0-9]+)$', views.patient_detail),
-    # url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
 ]
