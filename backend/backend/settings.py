3f33e8ed23894a0f68a6aea477975bd25fbd943b
Shallum99 <shallumisrael@gmail.com>
2022-01-15 16:20:43 +0530

    Doctor id and name added on prescription page

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
-    'corsheaders.middleware.CorsMiddleware',
     
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,8 +79,8 @@ TEMPLATES = [
     },
 ]
 
+# WSGI_APPLICATION = 'wsgi.application'
 WSGI_APPLICATION = 'backend.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
