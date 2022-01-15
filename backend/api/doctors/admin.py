fc9a9cb74be0452e921b10748438b4df1bdfd5b2
Sahil Verma <iamsahil1910@gmail.com>
2022-01-15 11:46:00 +0530

    doctor dashboard finished

--- backend/api/doctors/admin.py
+++ backend/api/doctors/admin.py
@@ -9,13 +9,13 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'firstname', 'lastname')}),
+        (None, {'fields': ('email', 'password', 'firstname', 'lastname', 'qualification', 'specialization', 'dob', 'phone_no', 'gender')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('firstname', 'lastname', 'email', 'password1', 'password2', 'qualification', 'specialization', 'dob', 'phone_no', 'gender', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email',)
