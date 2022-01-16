8bf35b78cee479644ca2752d01d1883472db5439
Sahil Verma <iamsahil1910@gmail.com>
2022-01-16 18:02:54 +0530

    sqlite3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,15 +88,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'healthcare',
-       'USER': 'health',
-       'PASSWORD': 'health@123',
-       'HOST': 'localhost',
-       'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
