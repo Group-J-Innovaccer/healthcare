eafe6665acd3a0c61b8e14151545286cc4ec5e22
bymayank <gargpitbull@gmail.com>
2022-01-17 09:59:32 +0530

    doctordashboard.js

--- frontend/src/components/DoctorDashboard.js
+++ frontend/src/components/DoctorDashboard.js
@@ -37,7 +37,7 @@ const DoctorDashboard = ({ isAuthenticated, setIsAuthenticated }) => {
             <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
             <div class="container-fluid" style={{
             width: "100%",
-            height: "100vh",
+            height: "100%",
             backgroundImage: "url('https://images.unsplash.com/photo-1611690828081-878ab4e8416d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=938&q=80')",
             backgroundPosition: 'center',
             backgroundSize: 'cover',
@@ -50,7 +50,7 @@ const DoctorDashboard = ({ isAuthenticated, setIsAuthenticated }) => {
             </div>
             
             <div className="container" style={{ display:"flex", justifyContent:"center",borderRadius:"10px 10px 10px 10px"}}>
-            <div className="card" style={{width: "35vw",height:"50vh", justifyContent:'center', borderRadius:"10px 10px 10px 10px"}}>
+            <div className="card" style={{width: "35vw",height:"50vh", justifyContent:'center', borderRadius:"10px 0 0 10px"}}>
                 <div className="card-body"style={{padding:'20px',borderRadius:"10px 0 0 10px", justifyContent:'center'}}>
                 
                 <h5>Doctor ID : <b>{doctorData.id}</b></h5><br />
@@ -70,13 +70,13 @@ const DoctorDashboard = ({ isAuthenticated, setIsAuthenticated }) => {
             </div>
 
 
-            <div className="container p-2" style={{ display:"flex", justifyContent:"center"}}>
+            <div className="container" style={{ display:"flex", justifyContent:"center"}}>
             <form id="contact-form" method="get" action="addprescription">
                 <div className="form-group"><button variant="outline-primary" style={{width:"20vw", height:"40px", backgroundColor:"#2a8fc7", border:"none", borderRadius:"10px", color:"white", fontWeight:"bolder"}}>Add Prescription</button></div>
 
                 </form>
             </div>
-            <div className="container p-2" style={{ display:"flex", justifyContent:"center"}}>
+            <div className="container" style={{ display:"flex", justifyContent:"center"}}>
             <form id="contact-form" method="get" action="addpatient">
                 <div className="form-group"><button variant="outline-primary" style={{width:"20vw", height:"40px", backgroundColor:"#2a8fc7", border:"none", borderRadius:"10px", color:"white", fontWeight:"bolder"}}>Add Patient</button></div>
 
